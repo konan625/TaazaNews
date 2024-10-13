cb78a07c41fcdbff4b5b3f04759f03e576267b0a
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2024-10-13 23:24:50 +0530

    Added fav-icon and logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 import { Link, Outlet } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function NavBar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-logo">
-                    <Link to="/"><span>TazaNews</span></Link>
-                </div>
-                <div className="navbar-right">
-                    <ul className="navbar-links">
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                    <SearchBar />
-                </div>
-            </nav>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <Link to="/">
+            <img src={logo} alt="Taaza News" className="logo" />
+          </Link>
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-links">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <SearchBar />
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
